1e5922b6d6dd46ff7934128a09ebd842830d13a1
school-trip <shashiprakash191@gmail.com>
2025-02-23 11:49:16 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,77 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js"
+import {uplodeOnCloudinary} from "../utils/cloudinay.js";
 
 const registerUser = asyncHandler (async (req, res) =>{
-    res.status(200).json({
-       massage: "ok"
-    })
-   
+
+const {fullname, email, password} = req.body
+console.log("email", email); 
+
+// if (fullname === "") {
+ //     throw new ApiError(400, "fullname is required");  // when we write like this we validate every one but we validate all in one code so use the method
+ // }
+if(
+    [fullname, email, password,username].some(() =>                      // we an check by using map function but we use some function here
+    field?.trim() === "")                       
+){
+    throw new ApiError(400, "All fields are required");
+}
+
+// this condition use for checking user is all ready exist or not 
+
+const existingUser =  User.findOne({
+    $or : [{username},{email}]
 })
 
 
+if(existingUser){
+    throw new ApiError(409, "User with this email already exists");
+}
 
-export {registerUser};
+const avatarLocalPath = req.files?.avatar[0]?.path;
+const coverImagePath = req.files?.cover[0]?.path;
+
+if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+}
+
+// uplode from cloudnary
+const avatar = await uplodeOnCloudinary(avatarLocalPath);
+const coverImage = await uplodeOnCloudinary(coverImagePath);
+
+if (!avatar) {
+    throw new ApiError(500, "Failed to uplode avatar to cloudinary");
+}
+
+// create user object entery in db
+
+const user = await User.create({
+    fullname,
+    email,
+    password,
+    username: username.toLowerCase(),
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "", // agar coverimage hai to thik na hai to empty chhhod do
+ })
+// check user is created or not 
+
+const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"              // sara field aae but password or refreshtoken na aaee
+)
+
+if (!createdUser) {
+    throw new ApiError(500, "Failed to create user");
+}
+
+// return response
+return res.status(201).json(
+    new ApiResponse(200, createdUser, "user created successfully")
+)
+
+
+});
+
+export {
+    registerUser,
+};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,24 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js ";
+import {uploded} from "../middlewares/multer.middleware.js";
 
 
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    uploded.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {                                      //it is used for img uplode 
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
 
 export default router;
 
